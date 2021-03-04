e36a840f28a436afc56c8eb077e96d8521e64cc6
Michael <michaelsilianov@gmail.com>
2021-03-04 12:07:53 +0200

    login is working

--- App/Controllers/Gui.py
+++ App/Controllers/Gui.py
@@ -4,16 +4,18 @@ from tkinter import *
 from tkinter import ttk
 
 from App.Controllers.Window import Window
+from App.Controllers.UIElements.LoginModal import LoginModal
 
 class Gui:
 
     def __init__(self):
-        root = Tk()
-        root.geometry("700x500")
-        root.title("Genetic App")
-        root.iconphoto(True, PhotoImage(file="App/Images/logo.png")) #why not showing up?
-        app = Window(root)
-        root.mainloop()
+        self.root = Tk()
+        self.root.geometry("700x500")
+        self.root.title("Genetic App")
+        self.root.iconphoto(True, PhotoImage(file="App/Images/logo.png")) #why not showing up?
 
+        self.lmodal = LoginModal(Window, self.root)
+        self.lmodal.create_modal()
 
+        self.root.mainloop()
 
