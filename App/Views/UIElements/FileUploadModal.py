a7364f3308d8e9699f3ee798658a2d16d76001d5
Michael <michaelsilianov@gmail.com>
2021-04-13 23:39:01 +0300

    changed file description input field

--- App/Views/UIElements/FileUploadModal.py
+++ App/Views/UIElements/FileUploadModal.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
-from tkinter import Entry, Toplevel, Button, Label
+import tkinter as tk
+from tkinter import Entry, Toplevel, Button, Label, Text
 from App.Services.Message import Message
 from tkinter import filedialog
 from App.Services.FileUploader import FileUploader
@@ -53,6 +54,11 @@ class FileUploadModal():
     def open_file_dialog(self):
         self.file_path = filedialog.askopenfilename()
 
+        self.file_name_entry.delete(0, tk.END)
+        self.file_name_entry.configure(state='normal')
+        self.file_name_entry.insert(0, self.file_path.split("/")[-1])
+        self.file_name_entry.configure(state='readonly')
+
     def set_file_dialog_button(self):
         self.file_dialog_button = Button(self.toplevel_dialog, text='Choose file', font=self.font, fg=self.fg, command=self.open_file_dialog)
         self.file_dialog_button.grid(row=1, column=1)
@@ -69,14 +75,14 @@ class FileUploadModal():
     def set_file_name_field(self):
         self.file_name_label = Label(self.toplevel_dialog, bg=self.bg, fg=self.fg, font=self.font, text="File name")
         self.file_name_label.grid(row=0, column=0)
-        self.file_name_entry = Entry(self.toplevel_dialog)
+        self.file_name_entry = Entry(self.toplevel_dialog, state='readonly')
         self.file_name_entry.grid(row=1, column=0)
 
     def set_file_description_field(self):
         self.file_description_label = Label(self.toplevel_dialog, bg=self.bg, fg=self.fg, font=self.font, text="Description")
         self.file_description_label.grid(row=2, column=0)
-        self.file_description_entry = Entry(self.toplevel_dialog)
-        self.file_description_entry.grid(row=3, column=0)
+        self.file_description_entry = Text(self.toplevel_dialog, height=3, width=6)
+        self.file_description_entry.grid(row=3, column=0, columnspan=2, sticky=tk.W+tk.E)
 
     def get_form_data(self):
 
@@ -89,7 +95,7 @@ class FileUploadModal():
             self.data =  {
                 'user_id': str(self.master.logged_user.id),
                 'file_name': self.file_name_entry.get(),
-                'file_description': self.file_description_entry.get(),
+                'file_description': self.file_description_entry.get("1.0",'end').strip(),
                 'file_created_at': datetime.now().strftime("%d/%m/%Y"),
                 'file_path': self.file_path
             }
@@ -112,7 +118,7 @@ class FileUploadModal():
 
     def check_file_description(self):
         try:
-            if(len(self.file_description_entry.get()) == 0):
+            if(len(self.file_description_entry.get("1.0",'end')) == 0):
                 return False
             
             return True
