05244da5f05268ddb4698752a453a9636453f110
Tamir <1hotlev@gmail.com>
2021-08-18 19:52:38 +0300

    GUI impovments

--- App/Views/UIElements/LoginModal.py
+++ App/Views/UIElements/LoginModal.py
@@ -1,6 +1,5 @@
 from typing import Any
-from tkinter import Entry, Toplevel, Button, Label, X, RIGHT, LEFT
-from App.Services.Message import Message
+from tkinter import Entry, Toplevel, Button, Label, X
 from App.Models.SimpleUser import SimpleUser
 from App.Controllers.Controller import Controller
 from App.Controllers.SettingsController import SettingsController
@@ -26,7 +25,7 @@ class LoginModal:
 
         SettingsController().set_view_settings(self)
 
-        self.title = "Login"
+        self.title = "Log In"
         self.logged_user = None
 
         self.create_modal()
@@ -49,7 +48,7 @@ class LoginModal:
         self.set_password_field()
 
         self.set_submit_button()
-        self.set_cancel_button()
+        # self.set_cancel_button()
 
     def close_modal(self) -> None:
         """
@@ -65,6 +64,7 @@ class LoginModal:
         """
         self.top_level_dialog = Toplevel(self.master, padx=5, pady=5)
         self.top_level_dialog.title(self.title)
+        self.top_level_dialog.config(bg='#9fdf9f')
         self.top_level_dialog.minsize(300, 150)
         self.top_level_dialog.transient(self.master)
         self.top_level_dialog.protocol("WM_DELETE_WINDOW", self.close_modal)
@@ -84,7 +84,7 @@ class LoginModal:
         """
         self.submit_button = Button(self.top_level_dialog, text='Submit', font=self.font, fg=self.fg,
                                     command=self.on_submit_login)
-        self.submit_button.pack(fill=X, expand=False, padx=5, pady=1)
+        self.submit_button.pack(fill=X, expand=False, padx=5, pady=10)
 
     def set_cancel_button(self) -> None:
         """
@@ -100,11 +100,11 @@ class LoginModal:
         create email field and label
         :return: None
         """
-        self.email_label = Label(self.top_level_dialog, bg=self.bg_modal, fg=self.fg, font=self.font, text="Email")
+        self.email_label = Label(self.top_level_dialog, bg='#9fdf9f', fg=self.fg, font=self.font, text="Email")
         self.email_label.pack(fill=X, expand=False, padx=5)
         
         self.email_entry = Entry(self.top_level_dialog)
-        self.email_entry.pack(fill=X, expand=False, padx=5)
+        self.email_entry.pack(fill=X, expand=False, padx=5, pady=5)
         # for dev
         self.email_entry.insert(0, 'email')
 
@@ -113,11 +113,11 @@ class LoginModal:
         create password field and label
         :return: None
         """
-        self.password_label = Label(self.top_level_dialog, bg=self.bg_modal, fg=self.fg, font=self.font, text="Password")
+        self.password_label = Label(self.top_level_dialog, bg='#9fdf9f', fg=self.fg, font=self.font, text="Password")
         self.password_label.pack(fill=X, expand=False, padx=5)
 
         self.password_entry = Entry(self.top_level_dialog, show='*')
-        self.password_entry.pack(fill=X, expand=False, padx=5)
+        self.password_entry.pack(fill=X, expand=False, padx=5, pady=5)
         # for dev
         self.password_entry.insert(0, '123')
 
